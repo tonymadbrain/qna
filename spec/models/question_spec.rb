d76dac55ebdfdb731165f682181db3b0a35438f6
Antony Ryabov <mb7222@yandex.ru>
2015-03-11 14:18:09 +0300

    use sh-m dependent

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,21 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
 
-  describe "validations and relationships" do
-    it { should validate_presence_of :title }
-    it { should validate_presence_of :body }
-    it { should have_many :answers }
-  end
-
-  describe "destroy questions" do
-    it "should destroy dependent answers" do
-      question = FactoryGirl.create(:question)
-      answer_1 = FactoryGirl.create(:answer, question_id: question.id)
-      answer_2 = FactoryGirl.create(:answer, question_id: question.id)
-      answer_3 = FactoryGirl.create(:answer, question_id: question.id)
-
-      expect { question.destroy! }.to change{ Answer.count }.by(-3)
-    end
-  end
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it { should have_many(:answers).dependent(:destroy) }
 
 end

32ab39210591263356cafa0e5804e0471fe90354
Antony Ryabov <mb7222@yandex.ru>
2015-03-11 10:12:52 +0300

    add tests for Question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+
 end

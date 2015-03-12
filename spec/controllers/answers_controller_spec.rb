d0a315d3edb62a65f344065425b937320c25d989
Antony Ryabov <mb7222@yandex.ru>
2015-03-12 13:29:35 +0300

    tests for basics answer

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -17,14 +17,84 @@ RSpec.describe AnswersController, type: :controller do
   end
 
   describe 'GET #edit' do
-    before { get :edit, question_id: question }
+    before { get :edit, question_id: question, id: answer }
 
     it 'assigns requested answer to @answer' do
       expect(assigns(:answer)).to eq @answer
     end
 
-    it 'render answer/edit' do
+    it 'render answer/edit view' do
       expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attr' do
+      it 'save new answer in database' do
+        expect { post :create, question_id: question, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirect to answer/show view' do
+        post :create, question_id: question, answer: attributes_for(:answer)
+        expect(response).to redirect_to answers_path(assigns(:answer).question)
+      end
+    end
+
+    context 'with invalid attr' do
+      it 'does not save new answer in database' do
+        expect { post :create, question_id: question, answer: attributes_for(:invalid_answer) }.to_not change(Question, :count)
+      end
+
+      it 're-render new view' do
+        post :create, question_id: question, answer: attributes_for(:invalid_answer)
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do 
+    context 'with valid attr' do
+      it 'assigns the requested answer with @answer' do
+        patch :update, question_id: question, id: answer, answer: attributes_for(:answer)
+        expect(assigns(:answer)).to eq answer
+      end
+
+      it 'change attr' do
+        patch :update, question_id: question, id: answer, answer: { body: "Test body" }
+        answer.reload
+        expect(answer.body).to eq "Test body"
+      end
+
+      it 'redirect to the updated answer' do
+        patch :update, question_id: question, id: answer, answer: attributes_for(:answer)
+        expect(response).to redirect_to answer
+      end
+    end
+
+    context 'with invalid attr' do
+      before { patch :update, question_id: question, id: answer, answer: { body: nil } }
+      
+      it 'does not change answer attr' do
+        answer.reload
+        expect(answer.body).to eq "Super answer text"
+      end
+
+      it 're-render edit view' do
+        expect(response).to render_template :edit
+      end
+    end 
+  end
+
+  describe 'DELETE #destroy' do
+    before { answer } 
+
+    it 'delete answer from database' do
+       expect { delete :destroy, question_id: question, id: answer }.to change(Question, :count).by(-1)
+     end
+
+    it 'redirect to answer/index view' do
+      delete :destroy, question_id: question, id: answer
+      expect(response).to redirect_to answers_path
     end
   end
 end

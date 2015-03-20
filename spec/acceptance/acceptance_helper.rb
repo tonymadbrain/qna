dd1271e2282d67a92d54fcdca03f5cf382834730
Antony Ryabov <mb7222@yandex.ru>
2015-03-20 15:29:38 +0300

    add acceptance_helper

---
+++ spec/acceptance/acceptance_helper.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.configure do |config|
+  config.include AcceptanceMacros, type: :feature
+
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
--- spec/acceptance/answers/create_answer_spec.rb
+++ spec/acceptance/answers/create_answer_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative '../acceptance_helper'
 
 feature 'Create answer', %q{
   For help user who ask question

efdf1371075f1d911522737597ceafb2e8f3fa01
Antony Ryabov <mb7222@yandex.ru>
2015-03-23 11:35:55 +0300

    add sample validates to models questions and answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,5 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   belongs_to :user
 
-  validates :title, :body, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 200 }
 end

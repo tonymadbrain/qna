cb838fffee2f33ff213dc0fd5340ec2f06b7f874
Antony Ryabov <mb7222@yandex.ru>
2015-03-28 14:04:56 +0300

    refactor user_id check in answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,13 +2,13 @@ class AnswersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :load_question
   before_action :load_answer, only: [:edit, :update, :destroy]
+  before_action :check_user, only: [:update, :destroy]
 
   def new
     @answer = @question.answers.new
   end
 
   def create
-    #@question = Question.find(params[:question_id])
     @answer = @question.answers.create(answer_params)
   end
 
@@ -17,17 +17,13 @@ class AnswersController < ApplicationController
   end
 
   def update
-    @answer.update(answer_params) if @answer.user_id == current_user.id
+    @answer.update(answer_params)
     @question = @answer.question
   end
 
   def destroy
-    if @answer.user_id == current_user.id
-      @answer.destroy
-      flash[:notice] = 'Answer successfully deleted.'
-    else
-      flash[:notice] = 'You cant delete this question.'
-    end
+    @answer.destroy
+    flash[:notice] = 'Answer successfully deleted.'
     redirect_to @answer.question
   end
 
@@ -42,8 +38,11 @@ class AnswersController < ApplicationController
   end
 
   def answer_params
-    #params.require(:answer).permit(:body)
     answer_params = params.require(:answer).permit(:body, :user_id)
     answer_params.merge( user_id: current_user.id )
+  end
+
+  def check_user
+    render text: 'You do not have permission to view this page.', status: 403 if @answer.user_id != current_user.id
   end
 end

da58b0701ad3fb4f2ee47484d75f6bb53218184d
Antony Ryabov <mb7222@yandex.ru>
2015-04-08 12:56:05 +0300

    fix views and test for assign user to answer

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -12,13 +12,9 @@ RSpec.describe AnswersController, type: :controller do
         expect { post :create, answer: attributes_for(:answer), question_id: question, format: :js }.to change(question.answers, :count).by(1)
       end
 
-      it 'assign user to answer.user_id' do
-        expect(answer.user_id).to eq user.id
-      end
-
-      it 'assign @user to answer.user_id' do
-        my_answer = Answer.create(body: answer.body, user: @user, question: question)
-        expect(my_answer.user_id).to eq @user.id
+      it 'assign user to created answer' do
+        post :create, answer: attributes_for(:answer), question_id: question, format: :js
+        expect(assigns(:current_user)).to eq @user
       end
 
       it 'render create template' do

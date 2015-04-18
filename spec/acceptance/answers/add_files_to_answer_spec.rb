e7f891884d214916aae9d81a4d93aed30176c441
Anton Ryabov <mb7222@yandex.ru>
2015-04-19 00:24:34 +0300

    Green tests for attach several files to answers

--- spec/acceptance/answers/add_files_to_answer_spec.rb
+++ spec/acceptance/answers/add_files_to_answer_spec.rb
@@ -4,6 +4,8 @@ feature 'Add files to answer' do
   
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
+  given(:first_file) { "#{Rails.root}/spec/spec_helper.rb" }
+  given(:second_file) { "#{Rails.root}/spec/rails_helper.rb" }
 
   background do
     log_in(user)
@@ -12,11 +14,26 @@ feature 'Add files to answer' do
 
   scenario 'User adds file when give answer', js: true do
     fill_in 'Answer', with: 'Test answer'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    attach_file 'File', first_file
     click_on 'Create'
 
     within '.answers' do
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end
+  end
+
+  scenario 'User attach several files when give answer', js: true do
+    fill_in 'Answer', with: 'Test answer'
+    click_on 'Add more'
+
+    file_inputs = page.all('input[type="file"]')
+    file_inputs[0].set first_file
+    file_inputs[1].set second_file
+    click_on 'Create'
+    save_and_open_page
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
     end
   end
 end

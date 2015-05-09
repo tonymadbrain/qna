8da508d34e45147af59aacc736c23e4f060c6d61
Anton Ryabov <mb7222@yandex.ru>
2015-05-10 01:38:25 +0300

    Delete extra line in test

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
 
   it { should belong_to(:question) }
-  it { should validate_presence_of :body }
-  it { should validate_presence_of :user }
+  it { should validate_presence_of :body and :user }
 
   describe 'Make answer best' do
     let(:user)      { create(:user) }

0ac035e6fe2bec02cef13daa0d832d727d93c474
Antony Ryabov <mb7222@yandex.ru>
2015-06-08 18:16:36 +0300

    Add default_from in production.env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.email_default_from = "notice@qna.doam.ru"
 end

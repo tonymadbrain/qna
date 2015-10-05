f3c1d5b066467676b281d365c7807da764d969eb
Antony Ryabov <mb7222@yandex.ru>
2015-10-05 21:18:16 +0300

    No need tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005135026) do
+ActiveRecord::Schema.define(version: 20151005181517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 20151005135026) do
 
   add_index "identities", ["provider", "uid"], name: "index_identities_on_provider_and_uid", using: :btree
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
+
+  create_table "multiplier_rating", force: :cascade do |t|
+    t.string   "rating_type"
+    t.string   "multiplier"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer  "resource_owner_id", null: false
@@ -112,22 +119,6 @@ ActiveRecord::Schema.define(version: 20151005135026) do
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
 
-  create_table "rating_actions", force: :cascade do |t|
-    t.integer  "rating_weight_id"
-    t.integer  "user_id"
-    t.integer  "count"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  add_index "rating_actions", ["rating_weight_id"], name: "index_rating_actions_on_rating_weight_id", using: :btree
-  add_index "rating_actions", ["user_id"], name: "index_rating_actions_on_user_id", using: :btree
-
-  create_table "rating_weights", force: :cascade do |t|
-    t.string  "name"
-    t.integer "weight"
-  end
-
   create_table "subscribe_lists", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "question_id"
@@ -171,8 +162,6 @@ ActiveRecord::Schema.define(version: 20151005135026) do
   add_foreign_key "answers", "questions"
   add_foreign_key "comments", "users"
   add_foreign_key "identities", "users"
-  add_foreign_key "rating_actions", "rating_weights"
-  add_foreign_key "rating_actions", "users"
   add_foreign_key "subscribe_lists", "questions"
   add_foreign_key "subscribe_lists", "users"
   add_foreign_key "votes", "users"

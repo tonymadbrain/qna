b4a1ce5cd529c81c5c1d0ed294e18390ce9a0d3f
Antony Ryabov <mb7222@yandex.ru>
2015-10-05 17:20:34 +0300

    Add migration for creating rating_actions

--- db/migrate/20151005134142_add_rating_weight.rb
+++ db/migrate/20151005134142_add_rating_weight.rb
@@ -1,6 +1,6 @@
 class AddRatingWeight < ActiveRecord::Migration
   def change
-    create_table :rating_weight do |t|
+    create_table :rating_weights do |t|
       t.string :name
       t.integer :weight
     end

e4b0690f86de0252d9f29cbb73ba883d52617979
Antony Ryabov <mb7222@yandex.ru>
2015-10-05 19:29:18 +0300

    First acceptance test for rating

--- spec/acceptance/rating.rb
+++ spec/acceptance/rating.rb
@@ -1,10 +1,22 @@
 require_relative 'acceptance_helper'
 
 feature 'User rating', type: :feature, js: true do
+  given(:user) { create(:user) }
+  given!(:question) { create :question, user: user }
 
   scenario 'When user answer on question, his rating increases on 1 point' do
-    skip "add some examples to (or delete) #{__FILE__}"
+    log_in(user)
+    visit question_path(question)
+
+    fill_in 'Answer', with: 'test answer'
+    click_on 'Create'
+
+    visit user_path(user)
+    within '#rating' do
+      expect(page).to have_content '1'
+    end
   end
+
   scenario 'When user vote for question, author of question receives 2 points' do
     skip "add some examples to (or delete) #{__FILE__}"
   end
